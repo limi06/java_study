24ba74827a2005bcb6609ac98d50047f7238798c
lsmi <1959871096@qq.com>
2023-01-30 10:48:08 +0800

    feat: 临时提交

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
     boolean aa = false;
 
-    public void  testPlay() {
+    public void testPlay() {
         System.out.println("test play");
     }
-
 }
